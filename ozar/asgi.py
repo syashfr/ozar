548d6592f3b82b466486d9e83c7fb7b7b3f12182
syashfr <syashfr@hotmail.com>
2020-09-27 13:56:49 +0200

    added marketplace as a pp

--- ozar/asgi.py
+++ ozar/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ozarsite project.
+ASGI config for ozar project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ozarsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ozar.settings')
 
 application = get_asgi_application()
--- ozar/wsgi.py
+++ ozar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ozarsite project.
+WSGI config for ozar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ozarsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ozar.settings')
 
 application = get_wsgi_application()
