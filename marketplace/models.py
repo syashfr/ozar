b7fa1e6166cef87de8cca085f6d5b0b65ba179b5
syashfr <syashfr@hotmail.com>
2020-10-23 13:46:41 +0200

    changed data model

--- marketplace/models.py
+++ marketplace/models.py
@@ -10,15 +10,11 @@ class Company(models.Model):
 
 
 class Service(models.Model):
-    name = models.CharField(max_length=200)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    category = models.CharField(max_length=200)
     desc = models.CharField(max_length=500)
     contact = models.CharField(max_length=15)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    price = models.FloatField(default=0.0)
-    reviews = models.CharField(max_length=1000)
+    price = models.FloatField(default=0.0, blank=True)
+    reviews = models.CharField(max_length=1000, blank=True)
     location = models.CharField(max_length=200)
-    category = models.CharField(max_length=200)
     main_image = models.ImageField(upload_to='services/', blank=True)
-
-    def __str__(self):
-        return self.name
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,8 @@
 from marketplace.models import Service
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, ListView
 
 
@@ -9,7 +11,10 @@ class MarketplaceHomepage(TemplateView):
 
 class MarketplaceSearchResult(ListView):
     template_name = 'search_results.html'
-    def get_queryset(self): # new
+
+    def get_queryset(self): 
         query = self.request.GET.get('q')
-        object_list = Service.objects.filter(name = query)
+        #get object list by category
+        object_list = Service.objects.filter(category = query)
         return object_list
+
